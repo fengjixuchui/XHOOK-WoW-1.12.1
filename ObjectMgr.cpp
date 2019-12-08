a730401d099cfdac4d8c4bbc3215dbb6f2a6aa12
maikel233 <maikel233@live.nl>
2019-12-08 19:46:11 +0100

    Delete ObjectMgr.cpp

--- ObjectMgr.cpp
+++
@@ -1,7 +0,0 @@
-#include "ObjectMgr.h"
-
-
-
-uint64(__cdecl* ObjectMgr::fpGetActivePlayerGuid)() = (ClntObjMgrGetActivePlayerGuidPtr)0x004D3790;
-CGObject_C* (__cdecl* ObjectMgr::fpGetObjectPtr)(uint64 objectGuid, TypeMask objectTypeMask, const char* file, int line) = (ClntObjMgrObjectPtr)0x004D4DB0;
-BOOL(__cdecl* ObjectMgr::fpEnumVisibleObjects)(VisibleObjectsEnumProc proc, void* param) = (ClntObjMgrEnumVisibleObjectsPtr)0x004D4B30;
